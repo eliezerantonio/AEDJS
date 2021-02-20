59402f2784133d504ab89076b3e162b5cb4d8ee5
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-20 22:07:58 +0100

    removendo elementos

--- 01-Arrays/js/array.js
+++ 01-Arrays/js/array.js
@@ -38,11 +38,16 @@ console.log(fibonacci) */
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 numbers.push(11);
+//adiciona depois
 
+//adiciona antes
 numbers.unshift(-1)
 numbers.unshift(-2)
 numbers.unshift(-3)
 numbers.unshift(-4)
 numbers.unshift(-5)
 
+numbers.pop(); //remove fo final
+
+numbers.shift()
 console.log(numbers);
