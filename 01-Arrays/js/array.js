0848fc98c3b5a269c68a481968531f51bf52bf01
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-20 21:44:03 +0100

    criando e inicializando array

---
+++ 01-Arrays/js/array.js
@@ -0,0 +1,15 @@
+var avgTemp = [];
+
+avgTemp[0] = 31.9;
+avgTemp[1] = 35.3;
+avgTemp[2] = 42.1;
+avgTemp[3] = 38;
+avgTemp[4] = 25.5;
+
+//console.log("Tempo " + avgTemp[2]);
+
+var daysOfWeek = new Array();
+var daysOfWeek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+
+console.log(daysOfWeek[3]);
