a3ca14c8a8e658aafd5c39ae5f31ec8408c410da
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-20 22:24:28 +0100

    acessando objectos bidimensinal

--- 01-Arrays/js/array.js
+++ 01-Arrays/js/array.js
@@ -58,11 +58,18 @@ console.log(numbers); */
 
 //Array bidimensinal
 
-var avgTempWeek = []
+var avgTempWeek = [] //-2
 var avgTempWeek1 = [33, 25.2, 19, 27, 23.4]
 var avgTempWeek2 = [41, 22.2, 20, 30, 26.4]
 
 avgTempWeek[0] = avgTempWeek1
 avgTempWeek[1] = avgTempWeek2
 
-console.log(avgTempWeek[0][4]);
+//console.log(avgTempWeek[0][4]);
+
+for (var i = 0; i < avgTempWeek.length; i++) {
+
+    for (var j = 0; j < avgTempWeek[i].length; j++) {
+        console.log(avgTempWeek[i][j])
+    }
+}
